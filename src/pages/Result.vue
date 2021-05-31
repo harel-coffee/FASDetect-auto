c8a20556a1e3b4a7d816551bbf51c71134fa2099
Lukas Ehrig <lukas.ehrig@student.hpi.uni-potsdam.de>
2021-06-01 00:35:34 +0200

    Add traffic lights again

--- src/pages/Result.vue
+++ src/pages/Result.vue
@@ -3,60 +3,25 @@
 
     <q-card flat bordered class="my-card bg-grey-1">
       <q-card-section class="text-center">
-        <div class="row">
-        <div class="col-11">
-<!--          <div class="text-h4">ERGEBNIS</div>-->
-          <div class="text-h3">ERGEBNIS</div>
-
-<!--          <div class="text-h6">Nach dem Screening besteht ein {{ risk_level }} RISIKO, dass der Patient an FASD leidet.</div>-->
-        </div>
-<!--        <div class="col-1">-->
-<!--          <q-icon :color=signal_color :name="risk_icon" size="xl" class="signal"></q-icon>-->
-<!--        </div>-->
-        </div>
+          <div class="text-h3 text-bold">ERGEBNIS</div>
       </q-card-section>
 
       <q-separator inset />
 
-      <q-card-section class="q-pt-xl q-pl-lg">
+      <q-card-section class="q-pt-xl q-pl-lg text-center">
 
-        <div class="text-bold">Nach dem Screening besteht ein <u>{{ risk_level }} RISIKO</u>, dass der Patient an FASD leidet.<p/></div>
+        <div class="text-bold text-h4 text-center">Es besteht ein <u>{{ risk_level }} RISIKO</u>, dass der Patient an FASD leidet.<p/></div>
 
-        <div class="row">
-
-          <div class="col-9 q-pl-lg">
-        Es bestehen Auffälligkeiten in folgenden Bereichen: <p/>
-
-          <div class="q-gutter-sm q-pl-md">
-            <q-checkbox disable v-model="growth" label="Wachstum" />
-    <!--        <q-checkbox disable v-model="face" label="Faciale Dysmorphie" />-->
-    <!--        <q-checkbox disable v-model="cns" label="Zentrales Nervensystem" />-->
-    <!--        <q-checkbox disable v-model="alcohol" label="Potentielle intrauterine Alkoholexposition" />-->
-          </div>
-          <div class="q-gutter-sm q-pl-md">
-            <q-checkbox v-model="face" @input="click(face)" disable label="Faciale Dysmorphie" />
-          </div>
-          <div class="q-gutter-sm q-pl-md">
-            <q-checkbox disable v-model="cns" label="Zentrales Nervensystem" />
-          </div>
-          <div class="q-gutter-sm q-pl-md">
-            <q-checkbox disable v-model="alcohol" label="Potentielle intrauterine Alkoholexposition" />
-          </div>
-
-            <p/>
-            <i>
-          <u>Was bedeutet dieses Ergebnis?</u> <p/>
-
-            Sensitivität des Tests : {{Math.round((this.$q.sessionStorage.getItem('prediction').scores.fasd.recall + Number.EPSILON) * 100) / 100}}%<br/>
-            Spezifität des Tests : {{Math.round((this.$q.sessionStorage.getItem('prediction').scores.adhs.recall + Number.EPSILON) * 100) / 100}}%<br/>
-            Zur Prävalenz von FAS(D) gibt es nur ungenaue Schätzungen, die von bis zu 5% der Bevölkerung ausgehen.
-            </i>
-          </div>
-
-          <div class="col-1">
-            <q-icon :color=signal_color :name="risk_icon" size="12vw" class="signal"></q-icon>
-          </div>
-
+        <div class="row q-pt-xl" style="height: 50vh">
+            <div class="col-5">
+            </div>
+            <div class="col">
+                  <q-img
+                    :src="risk_traffic_light_image">
+                  </q-img>
+            </div>
+            <div class="col-5">
+            </div>
         </div>
 
       </q-card-section>
@@ -71,13 +36,16 @@
       <q-dialog v-model="info">
         <q-card>
           <q-card-section>
-            <div class="text-h6">Ergebnis - Detailansicht</div>
+            <div class="text-h6 text-center">Ergebnis - Detailansicht</div>
           </q-card-section>
 
           <q-separator />
 
           <q-card-section style="max-height: 50vh" class="scroll">
-            <p v-for="n in 15" :key="n">Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum repellendus sit voluptate voluptas eveniet porro. Rerum blanditiis perferendis totam, ea at omnis vel numquam exercitationem aut, natus minima, porro labore.</p>
+            <p></p>
+            Sensitivität des Tests : {{Math.round((this.$q.sessionStorage.getItem('prediction').scores.fasd.recall + Number.EPSILON) * 100) / 100}}%<br/>
+            Spezifität des Tests : {{Math.round((this.$q.sessionStorage.getItem('prediction').scores.adhs.recall + Number.EPSILON) * 100) / 100}}%<br/>
+            Zur Prävalenz von FAS(D) gibt es nur ungenaue Schätzungen, die von bis zu 5% der Bevölkerung ausgehen.
           </q-card-section>
 
           <q-separator />
@@ -99,12 +67,17 @@
 .q-card
   width: 80%
   //height: 80vh
+  border-radius: 32px
 
-.signal
-  display: inline-block
-  border-radius: 600px
-  box-shadow: 0px 0px 8px #888
-  padding: 0.1em 0.1em
+.q-dialog
+  width: 80%
+  border-radius: 32px
+
+//.signal
+//  display: inline-block
+//  border-radius: 600px
+//  box-shadow: 0px 0px 8px #888
+//  padding: 0.1em 0.1em
 
 </style>
 
@@ -117,11 +90,7 @@ export default {
       risk_level: 'UNBEKANNTES',
       signal_color: 'white',
       risk_icon: '',
-      // risk_traffic_light_image: '',
-      growth: true,
-      face: false,
-      cns: false,
-      alcohol: false,
+      risk_traffic_light_image: '',
       info: false
     }
   },
@@ -132,15 +101,15 @@ export default {
     if (risk < 0.5) {
       this.risk_level = 'GERINGES'
       this.signal_color = 'green-7'
-      this.risk_icon = 'fas fa-check-circle'
+      this.risk_traffic_light_image = 'green.svg'
     } else if (risk >= 0.5 && risk < 0.75) {
       this.risk_level = 'ERHÖHTES'
       this.signal_color = 'yellow-7'
-      this.risk_icon = 'fas fa-exclamation-circle'
+      this.risk_traffic_light_image = 'yellow.svg'
     } else if (risk >= 0.75) {
       this.risk_level = 'HOHES'
       this.signal_color = 'red-7'
-      this.risk_icon = 'fas fa-exclamation-circle'
+      this.risk_traffic_light_image = 'red.svg'
     }
   },
 
